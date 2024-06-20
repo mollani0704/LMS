1d40029ea2a704fe14ed521f2960b4ad8a2d7cf3
mollani0704 <mollani0704@gmail.com>
2024-06-20 18:07:00 +0900

    [Add] JSP 연동

---
+++ src/main/java/com/study/lms/login/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.study.lms.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class LoginController {
+
+	@GetMapping("/")
+	public String home() {
+		log.info("===== Start LoginController Home");
+		log.info("===== End LoginController Home");
+		
+		return "index";
+	}
+	
+}
